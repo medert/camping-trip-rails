183420e315fe77e798b9dbfb323050d3374c781a
Meder <m.toktosunov@icloud.com>
2015-12-15 23:06:26 -0500

    Passing tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   resources :campers, only: [:index]
   resources :campsites, only: [:index]
   resources :supplies, only: [:index]
+
+  resources :campsites, only: [:show] do
+    resources :campers, only: [:index]
+  end
+
+  resources :campers, only: [:show] do
+    resources :supplies, only: [:index]
+  end
+
 end
